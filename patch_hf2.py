f823f5cb9cf3e5e5aa4d4359fd922c4ab40c61f6
IntelliBitz <muthu.ramadoss@gmail.com>
2026-09-19 20:54:55 +0530

    chore: remove accidental python temp scripts

--- patch_hf2.py
+++
@@ -1,9 +0,0 @@
-import re
-with open("src/gemi/hf_discovery.rs", "r") as f:
-    text = f.read()
-
-text = text.replace("step: step_idx,", "step: step_idx,\n                                fields: Default::default(),")
-text = text.replace("step.step = (i + 1) as u32;", "step.step = i + 1;")
-
-with open("src/gemi/hf_discovery.rs", "w") as f:
-    f.write(text)
